f25bc200685bd269ff480c829fc28905d4f996de
Tiago M. Neves <tiagomneves@protonmail.com>
2023-04-21 00:14:11 +0100

    Number of files changed added

--- rnmfl.sh
+++ rnmfl.sh
@@ -4,6 +4,7 @@
 
 NOME_CADEIRA=${PWD##*/}
 FICHEIROS=$(ls)
+NUM_ALTERACOES=0
 
 for FICHEIRO in $FICHEIROS 
 do
@@ -11,6 +12,14 @@ do
 	then
 		mv $FICHEIRO "[${NOME_CADEIRA}]- ${FICHEIRO}"
 	       	echo "A mudar o nome do ficheiro \"${FICHEIRO}\" para \"[${NOME_CADEIRA}]- ${FICHEIRO}\""
+		((NUM_ALTERACOES++))
 	fi
 done	
 
+if ((NUM_ALTERACOES != 0))
+then 
+	printf "Foram actualizados $NUM_ALTERACOES ficheiros."
+	exit 0
+fi
+
+printf "Nenhum ficheiro modificado."
