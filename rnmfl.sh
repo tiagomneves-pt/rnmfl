f4939c97b1a890c270858b88ced38199cc5cd1ee
Tiago M. Neves <tiagomneves@protonmail.com>
2023-04-21 11:10:49 +0100

    Fixed the bug that prevented the renaming of files with spaces

--- rnmfl.sh
+++ rnmfl.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 #WORK IN PROGRESS
-#Ficheiro de teste. Não utilizar fora da diretoria de teste.
 
 NOME_CADEIRA=${PWD##*/}
-FICHEIROS=$(ls)
 NUM_ALTERACOES=0
 
-for FICHEIRO in $FICHEIROS 
+for FICHEIRO in * 
 do
 	if [[ "$FICHEIRO" != "[$NOME_CADEIRA]"* ]]
 	then
-		mv $FICHEIRO "[${NOME_CADEIRA}]- ${FICHEIRO}"
+		mv "$FICHEIRO" "[${NOME_CADEIRA}]- ${FICHEIRO}"
 	       	echo "A mudar o nome do ficheiro \"${FICHEIRO}\" para \"[${NOME_CADEIRA}]- ${FICHEIRO}\""
 		((NUM_ALTERACOES++))
 	fi
